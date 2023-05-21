17005ec89b6945c890bc25e0d3ca6face17b5365
ayushdeveloper003 <119794831+ayushdeveloper003@users.noreply.github.com>
2023-05-21 11:29:33 +0530

    Create replit.nix

---
+++ replit.nix
@@ -0,0 +1,23 @@
+{ pkgs }: {
+
+    deps = [
+
+        pkgs.nodejs-16_x
+
+        pkgs.libwebp
+
+        pkgs.python
+
+	    pkgs.nodePackages.typescript        pkgs.libuuid
+
+        pkgs.ffmpeg
+
+        pkgs.imagemagick  
+
+        pkgs.wget
+
+        pkgs.git
+
+        pkgs.nodePackages.pm2
+
+    ];
